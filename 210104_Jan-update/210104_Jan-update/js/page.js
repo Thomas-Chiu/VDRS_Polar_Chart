bb5fcd01f2c305b5c6052a522e97235607e13eb0
Thomas-Chiu <josemich0511@gmail.com>
2021-03-16 22:27:54 +0800

    add Vina 's jquery example

---
+++ 210104_Jan-update/210104_Jan-update/js/page.js
@@ -0,0 +1,17 @@
+﻿$(function () {
+
+    var newHash = '',
+        $mainContent = $('.pageLoad');
+    $('.introduce-menu, .jobajax').delegate('a', 'click', function () {
+        $(window).scrollTop(0);
+        window.location.hash = $(this).attr('href');
+        $('.introduce-menu a.active').removeClass();
+        $(this).addClass('active');
+        return false;
+    });
+    $(window).bind('hashchange', function () {
+        newHash = window.location.hash.substr(1);
+        $mainContent.load(newHash + " > *");
+    });
+
+});
